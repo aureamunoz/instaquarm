1636f6e6bc8cf184763202110a891b26b6e0fa1e
Auri Munoz <amunozhe@redhat.com>
2023-03-29 16:13:43 +0200

    remove accessors and id because inherited from parent class

--- upload-service/src/main/java/org/instaquarm/uploading/Picture.java
+++ upload-service/src/main/java/org/instaquarm/uploading/Picture.java
@@ -2,53 +2,26 @@ package org.instaquarm.uploading;
 
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.annotations.Type;
-import org.hibernate.type.descriptor.jdbc.BinaryJdbcType;
 
 import java.sql.Types;
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-public class Picture  extends PanacheEntityBase {
+public class Picture  extends PanacheEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
-
-    private String title;
+    public String title;
 
     @CreationTimestamp
-    private Date created;
+    public Date created;
 
     @Lob
     @Column(columnDefinition = "BYTEA")
     @JdbcTypeCode(Types.BINARY)
-    private byte[] image;
+    public byte[] image;
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public Date getCreated() {
-        return created;
-    }
-
-    public byte[] getImage() {
-        return image;
-    }
 }
