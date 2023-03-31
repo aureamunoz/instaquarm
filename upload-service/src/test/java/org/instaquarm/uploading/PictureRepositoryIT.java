53f9ed2b89118ee031cef35457968d11ecefdfe7
Clement Escoffier <clement@apache.org>
2023-03-31 19:45:57 +0200

    Native build for the wall service

--- upload-service/src/test/java/org/instaquarm/uploading/PictureRepositoryIT.java
+++
@@ -1,8 +0,0 @@
-package org.instaquarm.uploading;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class PictureRepositoryIT extends PictureRepositoryTest {
-    // Execute the same tests but in packaged mode.
-}
