98f07f4c327f20fba89ca65cdf7b30474b330675
Auri Munoz <amunozhe@redhat.com>
2023-04-03 13:56:24 +0200

    refactor after review

--- upload-service/src/main/java/org/instaquarm/uploading/UploadController.java
+++ upload-service/src/main/java/org/instaquarm/uploading/UploadController.java
@@ -1,11 +1,10 @@
 package org.instaquarm.uploading;
 
 import jakarta.annotation.security.RolesAllowed;
-import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
-import org.instaquarm.uploading.client.SquarerRestService;
+import org.instaquarm.uploading.client.SquarerRestClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +17,8 @@ import java.util.List;
 @RequestMapping("/upload")
 public class UploadController {
 
-    @Inject
     @RestClient
-    SquarerRestService squareService;
+    SquarerRestClient squarerRestClient;
 
     public record PictureRequest(String title, String user, byte[] image) {};
 
@@ -35,8 +33,7 @@ public class UploadController {
         Picture picture = new Picture(request.title,request.user,request.image);
 
         Picture.persist(picture);
-        System.out.println("squareFunction called for picture");
-        squareService.makeItSquare(picture);
+        squarerRestClient.makeItSquare(picture);
         return Response.ok(picture).status(201).build();
     }
 }
--- upload-service/src/main/java/org/instaquarm/uploading/client/SquarerRestClient.java
+++ upload-service/src/main/java/org/instaquarm/uploading/client/SquarerRestClient.java
@@ -1,9 +1,7 @@
 package org.instaquarm.uploading.client;
 
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
@@ -11,10 +9,8 @@ import org.instaquarm.uploading.Picture;
 
 
 @RegisterRestClient(configKey = "squarer-function")
-public interface SquarerRestService {
+public interface SquarerRestClient {
 
     @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
     Picture makeItSquare(Picture squarePicture);
 }
